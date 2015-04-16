188e533f2076541c66160d997283d131c250541d
Henk Mollema <henkmollema@gmail.com>
2015-04-16 17:39:42 +0200

    Added Nancy webserver project with self hosting - #2.

---
+++ src/MindstormR.Server.Nancy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Nancy.Hosting.Self;
+
+namespace MindstormR.Server.Nancy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            using (var nancyHost = new NancyHost(new Uri("http://localhost:8888/")))
+            {
+                nancyHost.Start();
+
+                Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+                try
+                {
+                    Process.Start("http://localhost:8888/");
+                }
+                catch (Exception)
+                {
+                }
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+    }
+}
