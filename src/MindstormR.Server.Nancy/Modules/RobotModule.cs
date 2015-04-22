e417b4e4cd628211e349f7bf24477229220ddd5f
henkmollema <henkmollema@gmail.com>
2015-04-22 11:37:50 +0200

    Added RobotModule as endpoint for the robot - fixes #7

---
+++ src/MindstormR.Server.Nancy/Modules/RobotModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy;
+using System.Collections.Generic;
+
+namespace MindstormR.Client.Nancy
+{
+
+    public class RobotModule : NancyModule
+    {
+        private static List<int> _clients = new List<int>();
+        private static int _id = 1000;
+
+        public RobotModule()
+            : base("robot")
+        {
+            Get["/login"] = _ =>
+            {
+                int id = _id++;
+                _clients.Add(id);
+                return id.ToString();
+            };
+
+            Get["logout/{id:int}"] = _ =>
+            {
+                _clients.Remove(_.id);
+                return true.ToString();
+            };
+        }
+    }
+}
+
