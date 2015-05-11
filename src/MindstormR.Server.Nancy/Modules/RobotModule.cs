153a4b8cb02d908d9fc6d4d7d4e0031df6ab9cdb
henkmollema <henkmollema@gmail.com>
2015-05-11 15:04:26 +0200

    Added seperate methods for Login/Logout

--- src/MindstormR.Server.Nancy/Modules/RobotModule.cs
+++ src/MindstormR.Server.Nancy/Modules/RobotModule.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 namespace MindstormR.Client.Nancy
 {
-
     public class RobotModule : NancyModule
     {
         private static List<int> _clients = new List<int>();
@@ -13,18 +12,37 @@ namespace MindstormR.Client.Nancy
         public RobotModule()
             : base("robot")
         {
-            Get["/login"] = _ =>
-            {
-                int id = _id++;
-                _clients.Add(id);
-                return id.ToString();
-            };
+            Get["/login"] = Login;
 
-            Get["logout/{id:int}"] = _ =>
+            Get["logout/{id:int}"] = Logout;
+        }
+
+        private dynamic Login(dynamic parameters)
+        {
+            int id = _id++;
+            _clients.Add(id);
+            return id.ToString();
+        }
+
+        private dynamic Logout(dynamic parameters)
+        {
+            _clients.Remove(parameters.id);
+            return true.ToString();
+        }
+    }
+
+    public class RobotBase
+    {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RobotBase)
             {
-                _clients.Remove(_.id);
-                return true.ToString();
-            };
+                return Id == ((RobotBase)obj).Id;
+            }
+
+            return base.Equals(obj);
         }
     }
 }
