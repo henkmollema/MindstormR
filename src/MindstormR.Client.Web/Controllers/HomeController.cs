43cc211db083a99a9c17b33b08cead30050b2cfb
WesselKoorn <WesselKoorn@icloud.com>
2015-05-12 13:31:34 +0200

    Added a get function on the server whith the right command - #10

--- src/MindstormR.Client.Web/Controllers/HomeController.cs
+++ src/MindstormR.Client.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,7 +17,20 @@ namespace MindstormR.Client.Web.Controllers
 
         public JsonResult ControlClick(string command)
         {
-            return Json(new { success = true, command });
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://test.henkmollema.nl");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = client.GetAsync(string.Format("robot/{0}/{1}", "1000", command)).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true });
+                }
+            }
+
+            return Json(new { success = false });
         }
     }
 }
