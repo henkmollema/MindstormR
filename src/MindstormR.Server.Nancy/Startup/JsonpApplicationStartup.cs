fc285e27379b10ec1e2e62093b2a94de1f517182
henkmollema <henkmollema@gmail.com>
2015-05-18 15:42:33 +0200

    Enable JSONP responses

---
+++ src/MindstormR.Server.Nancy/Startup/JsonpApplicationStartup.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+
+namespace MindstormR.Client.Nancy
+{
+    /// <summary>
+    /// Enables JSONP support at application startup.
+    /// </summary>
+    public class JsonpApplicationStartup : IApplicationStartup
+    {
+        /// <summary>
+        /// Perform any initialisation tasks
+        /// </summary>
+        /// <param name="pipelines">Application pipelines</param>
+        public void Initialize(IPipelines pipelines)
+        {
+            Jsonp.Enable(pipelines);
+        }
+    }
+}
+
