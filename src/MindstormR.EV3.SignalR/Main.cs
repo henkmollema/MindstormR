563bdb331586d8fd97f1be4c00fdf2fdc277b579
henkmollema <henkmollema@gmail.com>
2015-06-17 16:15:29 +0200

    Playing around with SignalR

---
+++ src/MindstormR.EV3.SignalR/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using MonoBrickFirmware;
+using MonoBrickFirmware.Display.Dialogs;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Movement;
+using System.Threading;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace MonoBrickHelloWorld
+{
+    class MainClass
+    {
+        private static int _id;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                new InfoDialog("Creating hub..", false).Show();
+                var connection = new HubConnection("http://mindstormr.novucura.com/");
+
+                new InfoDialog("Creating hub proxy.", false).Show();
+                var hub = connection.CreateHubProxy("MindstormrHub");
+
+                hub.On<int>("LoggedIn", id =>
+                    {
+                        _id = id;
+                        new InfoDialog("Logged in: " + _id, true).Show();
+                    });
+
+
+                new InfoDialog("Connecting...", false).Show();
+
+                // Set the connection timeout to 5 minutes.
+                connection.TransportConnectTimeout = TimeSpan.FromSeconds(300);
+                var sw = Stopwatch.StartNew();
+                connection.Start().Wait(TimeSpan.FromSeconds(300));
+                sw.Stop();
+
+                new InfoDialog(string.Format("Connected in {0:n2}s", sw.Elapsed.TotalSeconds), true).Show();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.Restart();
+                    hub.Invoke("PushCommand", _id, "forward");
+                    sw.Stop();
+                    new InfoDialog(string.Format("Pushed command in {0:n2}ms", sw.Elapsed.TotalMilliseconds), true).Show();
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.Restart();
+                    hub.Invoke("GetCommand", _id);
+                    sw.Stop();
+                    new InfoDialog(string.Format("Retrieved command in {0:n2}ms", sw.Elapsed.TotalMilliseconds), true).Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                LcdConsole.Clear();
+                LcdConsole.WriteLine(ex.Message);
+                LcdConsole.WriteLine("");
+            }
+        }
+    }
+}
---
+++ src/MindstormR.Server.SignalR/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace MindstormR.Server.SignalR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://*:8888";
+            var app = WebApp.Start(url);
+
+            Console.WriteLine("Server running on {0}", url);
+
+            // Under mono if you daemonize a process a Console.ReadLine will cause an EOF 
+            // so we need to block another way.
+            if (args.Any(s => s.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                Thread.Sleep(Timeout.Infinite);
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+
+            app.Dispose();
+            app = null;
+            Console.WriteLine("Hosting SignalR stopped");
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage(Microsoft.Owin.Diagnostics.ErrorPageOptions.ShowAll);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR(new HubConfiguration { EnableDetailedErrors = true });
+        }
+    }
+
+    public class MindstormrHub : Hub
+    {
+        private static int _id = 1000;
+        private static Dictionary<string, int> _clients = new Dictionary<string, int>();
+
+        public override Task OnConnected()
+        {
+            string conId = Context.ConnectionId;
+            int id = _id++;
+            _clients.Add(conId, id);
+
+            Clients.Caller.LoggedIn(id);
+
+            return base.OnConnected();
+        }
+    }
+}
